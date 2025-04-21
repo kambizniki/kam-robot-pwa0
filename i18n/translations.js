00b3127af49dfa13cdca370a95299ce05863a9a8
kambizniki <kambiz.niknafs@gmail.com>
2025-04-22 01:38:25 +0330

    Create translations.js

---
+++ i18n/translations.js
@@ -0,0 +1,18 @@
+export const translations = {
+  en: {
+    welcome: "Welcome to Kam Robot",
+    addAccount: "Add XT Account",
+    gridBotSettings: "Grid Bot Settings",
+    startBot: "Start Bot",
+    profitLoss: "Profit / Loss",
+    switchLanguage: "Switch to Persian"
+  },
+  fa: {
+    welcome: "به ربات کم خوش آمدید",
+    addAccount: "افزودن حساب XT",
+    gridBotSettings: "تنظیمات ربات گرید",
+    startBot: "شروع ربات",
+    profitLoss: "سود / زیان",
+    switchLanguage: "تغییر زبان به انگلیسی"
+  }
+};
